20e91174197cbdda914e37056aa0d309e26a3af2
Brian Staber <brian.staber@gmail.com>
2025-02-20 23:55:32 +0100

    Added the constrained tempered SMC method without adaptation

--- blackjax/smc/__init__.py
+++ blackjax/smc/__init__.py
@@ -1,9 +1,10 @@
-from . import adaptive_tempered, inner_kernel_tuning, tempered
+from . import adaptive_tempered, inner_kernel_tuning, tempered, constrained_tempered
 from .base import extend_params
 
 __all__ = [
     "adaptive_tempered",
     "tempered",
+    "constrained_tempered",
     "inner_kernel_tuning",
     "extend_params",
     "partial_posteriors_path",
